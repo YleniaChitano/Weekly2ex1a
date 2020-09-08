790032dc173067462aa52d418399e734716c22eb
YleniaChitano <116096@stud.inn.no>
2020-09-08 13:23:08 +0200

    Add project files.

---
+++ Weekly2ex1a/Weekly2ex1a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int numbers{ 1 };
+    while (numbers <= 100) {
+        std::cout << numbers << "\n";
+        numbers++;
+    }
+    
+
+    return 0;
+}
+
